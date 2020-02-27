c26aa746fc09acc86cbf9e24104b145175226ecc
Britney Parkerson <justbritneyp1989@gmail.com>
2020-02-27 00:45:06 -0500

    Code for forms. Having Dependency/Import issues. Will move to correct files when fixed.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+// import {Route} from "react-dom"
 import './App.css';
+import './Login.js';
 
 function App() {
+  const [signIn, setSignIn] = useState("");
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Code for Forms.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
-    </div>
-  );
-}
+        {console.log(signIn)}
+        <form>
+        <label><h1> Register</h1></label>
+          <label>
+           Full Name:
+            <input type="text" placeholder="First Name" onChange={event => setSignIn(event.target.value)} />
+            .<input type="text" placeholder="Last Name" onChange={event => setSignIn(event.target.value)} />
+           <br></br>
+            Email Address (Username) :
+            <input type="text" placeholder="Email Address" onChange={event => setSignIn(event.target.value)} />
+           <br></br>
+            Password:
+            <input type="text" placeholder="Password" onChange={event => setSignIn(event.target.value)} />
+            <br></br><br></br>*Passwords are case sensitive*
+            <br></br>*Passwords Requirements*
+            <br></br> *Must be 8 or more characters*
+            <br></br> *Must include at least 1 number, 1 uppercase 
+            <br></br> and 1 lowercase letter, and a symbol*
+            <br></br><br></br> Confirm Password:
+            <input type="text" placeholder="Confirm Password" onChange={event => setSignIn(event.target.value)} />
+            
+            
+          </label>
+        </form>
+      <br></br>
+      <br></br>
+      <br></br>
+        <form>
+        <label><h1> Login</h1></label>
+          <label>
+            Username (Email):
+            <input type="text" placeholder="Username/Email" onChange={event => setSignIn(event.target.value)} />
+           </label>
+           </form>
+           <form>
+           <label>
+            Password:
+            <input type="text" placeholder="Password" onChange={event => setSignIn(event.target.value)} />
+            <br></br>*Passwords are case sensitive*
+          </label>
+        </form>
 
-export default App;
+      </div>
+    );
+  }
+  
+  export default App;
