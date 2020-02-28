add3fbc99b55ac08d4130b3120dc3c319103cfa6
Ben Koehler <bkoehler85282@gmail.com>
2020-02-27 18:17:21 -0700

    removed store as not working

--- src/index.js
+++ src/index.js
@@ -2,23 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { reducer } from "./reducers";
-import "bootstrap/dist/css/bootstrap.min.css";
 import * as serviceWorker from "./serviceWorker";
-
-import { applyMiddleware, createStore } from "redux";
-import { Provider } from "react-redux";
-import thunk from "react-redux";
-import logger from "redux-logger";
-
-const store = createStore(reducer, applyMiddleware(thunk, logger));
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+import "bootstrap/dist/css/bootstrap.min.css";
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
