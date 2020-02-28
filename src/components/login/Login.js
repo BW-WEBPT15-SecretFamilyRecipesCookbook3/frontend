5bd4a62ee006b4db42c1ca1eb9be84491497cde5
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2020-02-27 21:59:05 -0500

    moved form, set router in index

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,23 +1,88 @@
 import React, { useState } from 'react';
-// import './App.css';
+//import axios from 'axios';
+
 
 
 function SignIn() {
-  const [name, setName] = useState("");
+
+  const [signIn, setSignIn] = useState("");
 
   return (
     <div className="App">
-      {console.log(name)}
+      <header className="App-header">
+        <p>Code for Forms.</p>
+      </header>
+      {console.log(signIn)}
       <form>
         <label>
-          Username (Email):
-          <input type="text" onChange={event => setName(event.target.value)} />
-         </label>
-         </form>
-         <form>
-         <label>
+          <h1> Register</h1>
+        </label>
+        <label>
+          Full Name:
+          <input
+            type="text"
+            placeholder="First Name"
+            onChange={event => setSignIn(event.target.value)}
+          />
+          .
+          <input
+            type="text"
+            placeholder="Last Name"
+            onChange={event => setSignIn(event.target.value)}
+          />
+          <br></br>
+          Email Address (Username) :
+          <input
+            type="text"
+            placeholder="Email Address"
+            onChange={event => setSignIn(event.target.value)}
+          />
+          <br></br>
           Password:
-          <input type="text" onChange={event => setName(event.target.value)} />
+          <input
+            type="text"
+            placeholder="Password"
+            onChange={event => setSignIn(event.target.value)}
+          />
+          <br></br>
+          <br></br>*Passwords are case sensitive*
+          <br></br>*Passwords Requirements*
+          <br></br> *Must be 8 or more characters*
+          <br></br> *Must include at least 1 number, 1 uppercase
+          <br></br> and 1 lowercase letter, and a symbol*
+          <br></br>
+          <br></br> Confirm Password:
+          <input
+            type="text"
+            placeholder="Confirm Password"
+            onChange={event => setSignIn(event.target.value)}
+          />
+        </label>
+      </form>
+      <br></br>
+      <br></br>
+      <br></br>
+      <form>
+        <label>
+          <h1> Login</h1>
+        </label>
+        <label>
+          Username (Email):
+          <input
+            type="text"
+            placeholder="Username/Email"
+            onChange={event => setSignIn(event.target.value)}
+          />
+        </label>
+      </form>
+      <form>
+        <label>
+          Password:
+          <input
+            type="text"
+            placeholder="Password"
+            onChange={event => setSignIn(event.target.value)}
+          />
           <br></br>*Passwords are case sensitive*
         </label>
       </form>
