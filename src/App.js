8bc4bfcc555c7116c007c62c0294fdbd27a341f0
Britney Parkerson <justbritneyp1989@gmail.com>
2020-02-27 21:58:46 -0500

    Save

--- src/App.js
+++ src/App.js
@@ -1,90 +1,22 @@
-import React, { useState } from "react";
-// import {Route} from "react-dom"
+import React from "react";
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import "./App.css";
-import "./Login.js";
+import Login from "./components/login/Login.js";
+import Register from "./components/login/Register.js";
 
 function App() {
-  const [signIn, setSignIn] = useState("");
   return (
     <div className="App">
       <header className="App-header">
         <p>Code for Forms.</p>
       </header>
-      {console.log(signIn)}
-      <form>
-        <label>
-          <h1> Register</h1>
-        </label>
-        <label>
-          Full Name:
-          <input
-            type="text"
-            placeholder="First Name"
-            onChange={event => setSignIn(event.target.value)}
-          />
-          .
-          <input
-            type="text"
-            placeholder="Last Name"
-            onChange={event => setSignIn(event.target.value)}
-          />
-          <br></br>
-          Email Address (Username) :
-          <input
-            type="text"
-            placeholder="Email Address"
-            onChange={event => setSignIn(event.target.value)}
-          />
-          <br></br>
-          Password:
-          <input
-            type="text"
-            placeholder="Password"
-            onChange={event => setSignIn(event.target.value)}
-          />
-          <br></br>
-          <br></br>*Passwords are case sensitive*
-          <br></br>*Passwords Requirements*
-          <br></br> *Must be 8 or more characters*
-          <br></br> *Must include at least 1 number, 1 uppercase
-          <br></br> and 1 lowercase letter, and a symbol*
-          <br></br>
-          <br></br> Confirm Password:
-          <input
-            type="text"
-            placeholder="Confirm Password"
-            onChange={event => setSignIn(event.target.value)}
-          />
-        </label>
-      </form>
-      <br></br>
-      <br></br>
-      <br></br>
-      <form>
-        <label>
-          <h1> Login</h1>
-        </label>
-        <label>
-          Username (Email):
-          <input
-            type="text"
-            placeholder="Username/Email"
-            onChange={event => setSignIn(event.target.value)}
-          />
-        </label>
-      </form>
-      <form>
-        <label>
-          Password:
-          <input
-            type="text"
-            placeholder="Password"
-            onChange={event => setSignIn(event.target.value)}
-          />
-          <br></br>*Passwords are case sensitive*
-        </label>
-      </form>
+      <Router>
+      <Route exact path ='/login' component={Login}/>
+      <Route exact path ='/register' component={Register}/>
+      </Router>
     </div>
+    
   );
 }
 
