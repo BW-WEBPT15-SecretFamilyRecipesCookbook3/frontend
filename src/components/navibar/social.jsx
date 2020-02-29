2b93a18ad2960037943dbde0f87758fc1e0e7057
Ben Koehler <bkoehler85282@gmail.com>
2020-02-28 19:49:10 -0700

    fixed issue with font-icons

--- src/components/navibar/social.jsx
+++ src/components/navibar/social.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import { Logout, YouTube, Twitter } from '../AppComponents/AllBtn';
-import './navi.css';
-
+import React from "react";
+import { Logout, YouTube, Twitter } from "../AppComponents/AllBtn";
+import "./navi.css";
 
 const Social = () => {
-    return(
-        <div className="social-wrap">
-            <Twitter />
-            <YouTube />
-            <Logout />
-        </div>
-    )
-}
+  return (
+    <div className="social-wrap">
+      <Twitter />
+      <YouTube />
+      <Logout />
+    </div>
+  );
+};
 export default Social;
