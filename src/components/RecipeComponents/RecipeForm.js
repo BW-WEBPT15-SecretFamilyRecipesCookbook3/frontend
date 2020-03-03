46d9cea47f979fce26f659b1e31d51c8e7f003a2
Ben Koehler <bkoehler85282@gmail.com>
2020-03-02 21:49:19 -0700

    fixed issue with adding recipe

--- src/components/RecipeComponents/RecipeForm.js
+++ src/components/RecipeComponents/RecipeForm.js
@@ -10,7 +10,6 @@ import {
   // Button,
   Form,
   FormGroup,
-  Label,
   Input,
   Button
   // FormText
@@ -20,22 +19,7 @@ class RecipeForm extends React.Component {
   state = {
     title: "",
     description: "",
-    tags: [],
-    tag: "",
-    commonTags: [
-      "Breakfast",
-      "Lunch",
-      "Dinner",
-      "Dessert",
-      "Side",
-      "Main",
-      "Appetizer",
-      "Vegetable",
-      "Chicken",
-      "Pork",
-      "Beef",
-      "Quick"
-    ]
+    source: ""
   };
 
   handleChanges = e => {
@@ -46,65 +30,12 @@ class RecipeForm extends React.Component {
     });
   };
 
-  addIngredient = e => {
-    e.preventDefault();
-    this.setState(state => {
-      const ingredients = [...state.ingredients, state.ingredientValue];
-      return {
-        ingredients,
-        ingredientValue: ""
-      };
-    });
-  };
-
-  addDirection = e => {
-    e.preventDefault();
-    this.setState(state => {
-      const directions = [...state.directions, state.directionValue];
-      return {
-        directions,
-        directionValue: ""
-      };
-    });
-  };
-
-  addTagByButton = (e, tag) => {
-    e.preventDefault();
-    this.setState(state => {
-      const tags = [...state.tags, tag.toString()];
-      const commonTags = state.commonTags.filter(el => el !== tag);
-      return {
-        tags,
-        commonTags
-      };
-    });
-  };
-  addCustomTag = e => {
-    e.preventDefault();
-    const newTags = [...this.state.tags];
-    newTags.push(this.state.tag);
-    this.setState({
-      tags: newTags,
-      tag: ""
-    });
-  };
-
-  deleteTag = (e, index) => {
-    e.preventDefault();
-    const newTags = [...this.state.tags];
-    newTags.splice(index, 1);
-    this.setState({
-      tags: newTags
-    });
-  };
-
   submitRecipe = e => {
     e.preventDefault();
     const newRecipe = {
       title: this.state.title,
       description: this.state.description,
-      source: this.state.source,
-      tags: this.state.tags
+      source: this.state.source
     };
     console.log("submit recipe history", this.props.history);
     this.props.addRecipe(newRecipe, this.props.history);
@@ -116,7 +47,7 @@ class RecipeForm extends React.Component {
         <Form onSubmit={this.submitRecipe}>
           <h2>Create New Recipe</h2>
           <Row form>
-            <Col md={9}>
+            <Col md={15}>
               <FormGroup className="create-recipe">
                 {/* <Label 
               for="Create-New-Recipe"
@@ -149,36 +80,6 @@ class RecipeForm extends React.Component {
             </Col>
           </Row>
 
-          <div className="tags-wrapper">
-            <h3>Tags</h3>
-            <div className="tags">
-              {this.state.commonTags.map((tag, index) => {
-                return (
-                  <button
-                    key={index}
-                    onClick={e => this.addTagByButton(e, tag)}
-                  >
-                    {tag}
-                  </button>
-                );
-              })}
-              <input
-                type="text"
-                name="tag"
-                onChange={this.handleChanges}
-                value={this.state.tag}
-              />
-              <button onClick={this.addCustomTag}>Add Custom Tag</button>
-              {this.state.tags.map((tag, index) => (
-                <div className="tag">
-                  <p>{tag}</p>
-                  <button onClick={e => this.deleteTag(e, index)}>
-                    Delete Tag
-                  </button>
-                </div>
-              ))}
-            </div>
-          </div>
           <Button type="submit"> Add Recipe</Button>
         </Form>
       </div>
