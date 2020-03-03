17f580611bb5e9ba05b75cadbfd38abac2467ae0
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2020-03-02 22:20:32 -0500

    logout function created in social

--- src/components/navibar/social.jsx
+++ src/components/navibar/social.jsx
@@ -3,11 +3,17 @@ import { Logout, YouTube, Twitter } from "../AppComponents/AllBtn";
 import "./navi.css";
 
 const Social = () => {
+
+  const LogOut = () => {
+    localStorage.setItem('token', "");
+    console.log('clicked');
+  }
+
   return (
     <div className="social-wrap">
       <Twitter />
       <YouTube />
-      <Logout />
+      <Logout onClick={LogOut()}/>
     </div>
   );
 };
