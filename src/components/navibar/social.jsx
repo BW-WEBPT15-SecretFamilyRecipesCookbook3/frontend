ebf753d3e0a47170e1464e33790a5cb9cc1a8601
Ben Koehler <bkoehler85282@gmail.com>
2020-03-03 18:26:37 -0700

    fixed issue with token

--- src/components/navibar/social.jsx
+++ src/components/navibar/social.jsx
@@ -3,19 +3,18 @@ import { Logout, YouTube, Twitter } from "../AppComponents/AllBtn";
 import "./navi.css";
 
 const Social = () => {
-
   const LogOut = () => {
-    localStorage.setItem('token', "");
-    
-    console.log('clicked');
-  }
+    localStorage.setItem("token", "");
+
+    console.log("clicked");
+  };
 
   return (
     <div className="social-wrap">
       <Twitter />
       <YouTube />
       <a href="https://cookbookproject.netlify.com/">
-        <Logout onClick={LogOut()}/>
+        <Logout onClick={LogOut()} />
       </a>
     </div>
   );
