780e20c4cbdab608258fef66c1d134ccd712409d
Ben Koehler <bkoehler85282@gmail.com>
2020-03-03 18:38:02 -0700

    fixed issue with token

--- src/components/RecipeComponents/RecipeForm.js
+++ src/components/RecipeComponents/RecipeForm.js
@@ -19,7 +19,9 @@ class RecipeForm extends React.Component {
   state = {
     title: "",
     description: "",
-    source: ""
+    source: "",
+    ingredients: [],
+    directions: []
   };
 
   handleChanges = e => {
@@ -29,13 +31,52 @@ class RecipeForm extends React.Component {
       [e.target.name]: e.target.value
     });
   };
+  addIngredient = e => {
+    e.preventDefault();
+    this.setState(state => {
+      const ingredients = [...state.ingredients, state.ingredientValue];
+      return {
+        ingredients,
+        ingredientValue: ""
+      };
+    });
+  };
+  addDirection = e => {
+    e.preventDefault();
+    this.setState(state => {
+      const directions = [...state.directions, state.directionValue];
+      return {
+        directions,
+        directionValue: ""
+      };
+    });
+  };
+
+  deleteIngredient = (e, index) => {
+    e.preventDefault();
+    const newIngredients = [...this.state.ingredients];
+    newIngredients.splice(index, 1);
+    this.setState({
+      ingredients: newIngredients
+    });
+  };
+  deleteDirection = (e, index) => {
+    e.preventDefault();
+    const newDirections = [...this.state.directions];
+    newDirections.splice(index, 1);
+    this.setState({
+      directions: newDirections
+    });
+  };
 
   submitRecipe = e => {
     e.preventDefault();
     const newRecipe = {
       title: this.state.title,
       description: this.state.description,
-      source: this.state.source
+      source: this.state.source,
+      ingredients: this.state.ingredients,
+      instructions: this.state.directions
     };
     console.log("submit recipe history", this.props.history);
     this.props.addRecipe(newRecipe, this.props.history);
@@ -69,6 +110,49 @@ class RecipeForm extends React.Component {
                   onChange={this.handleChanges}
                   value={this.state.source}
                 />
+                <Input
+                  placeholder="Ingredient"
+                  type="text"
+                  name="ingredientValue"
+                  onChange={this.handleChanges}
+                  value={this.state.ingredientValue}
+                />
+                <button onClick={this.addIngredient}>Add Ingredient</button>
+                {this.state.ingredients.map((ingredient, index) => (
+                  <div className="ingredient">
+                    <ShowArrayItem
+                      listNum={index + 1}
+                      item={ingredient}
+                      key={index}
+                    />
+                    <button onClick={e => this.deleteIngredient(e, index)}>
+                      Delete Ingredient
+                    </button>
+                  </div>
+                ))}
+                <div className="directions-wrapper">
+                  <h3>Directions</h3>
+                  <input
+                    type="text"
+                    name="directionValue"
+                    onChange={this.handleChanges}
+                    value={this.state.directionValue}
+                    placeholder="Direction"
+                  />
+                  <button onClick={this.addDirection}>Plus</button>
+                  {this.state.directions.map((direction, index) => (
+                    <div className="direction">
+                      <ShowArrayItem
+                        listNum={index + 1}
+                        item={direction}
+                        key={index}
+                      />
+                      <button onClick={e => this.deleteDirection(e, index)}>
+                        Delete Direction
+                      </button>
+                    </div>
+                  ))}
+                </div>
                 <Input
                   placeholder="Description"
                   type="text"
