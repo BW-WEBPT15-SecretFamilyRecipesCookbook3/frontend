aaa7fd833a022a9f3e8cd6cb5a1cd2a8461e52a4
Mason52171 <44660994+Mason52171@users.noreply.github.com>
2020-03-10 08:54:45 -0400

    Log out on dashboard works again. Will remove token and send home.

--- src/components/navibar/social.jsx
+++ src/components/navibar/social.jsx
@@ -1,23 +1,36 @@
-import React from "react";
-import { Logout, YouTube, Twitter } from "../AppComponents/AllBtn";
+import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom';
+import { YouTube, Twitter } from "../AppComponents/AllBtn";
 import "./navi.css";
 
 const Social = () => {
 
+  const [log, setLog] = useState(false);
+
   const LogOut = () => {
     localStorage.removeItem('token', "");
-    
+    setLog(false)
     console.log('clicked');
   }
   
- 
+  useEffect(() => {
+    const login = localStorage.getItem('token');
+      
+      if(login !== null) {
+        setLog(true);
+      }
+  }, [])
 
   return (
     <div className="social-wrap">
       
       <Twitter />
       <YouTube />
-        <Logout onClick={LogOut} />
+      { 
+        log 
+        ? <Link className="logout" onClick={LogOut} to='/' >Log Out</Link>
+        : <Link to='/login' >Log In</Link>
+      }
          
     </div>
   );
